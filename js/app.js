87eb1315a68b70dbde9667c28aeaa16f94a73524
shway250 <jackson_duhon@yahoo.com>
2016-07-24 17:07:14 -0700

    built game board, made squares clickable

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Hello frontend');
+/*
+Jackson
+*/
+
+var blankDiv = document.getElementsByClassName('playSquare');
+
+blankDiv.addEventListener("click", function(){
+	for(int i = 0; i < blankDiv.length; i++){
+		blankDiv[i].style.backgroundColor = "blue";
+	}
+});
